cd3364bac3310bd457f32c773e21d8f8e5fd489c
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-05-10 11:49:29 -0400

    React-Setup-v0.0.3

---
+++ src/components/dummy.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Dummy extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        )
+    }
+}
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,15 @@
 import { combineReducers } from "redux";
+import { dummyReducer } from "./dummy.reducer"
 
+
+export interface IDumState {
+    dumMessage?: String
+}
 
 export interface IState {
-
+    dum: IDumState
 }
 
 export const state = combineReducers<IState>({
-
+    dum: dummyReducer
 })
