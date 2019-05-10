46e9ceec350bd481b19d983f7c5ec7ee69e08678
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-05-10 11:07:01 -0400

    React/Redux Clean

--- src/model/role.ts
+++
@@ -1,9 +0,0 @@
-export class Role {
-    role: string;
-    roleId: number;
-
-        constructor(role='', roleId=0) {
-            this.role = role;
-            this.roleId = roleId;
-        }
-}
