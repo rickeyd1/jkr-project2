d35321031665ed9abe2edb82d1e373d5a378c3eb
Ric Davis <rickjay0@gmail.com>
2019-05-13 09:37:06 -0400

    Added dto package with Login.java

--- Tasty/src/main/java/com/revature/controllers/UserController.java
+++ Tasty/src/main/java/com/revature/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.revature.controllers;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.model.Login;
+import com.revature.dto.Login;
 import com.revature.model.Users;
 import com.revature.services.UserService;
 
